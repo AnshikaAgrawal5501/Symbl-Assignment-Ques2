4e4525fef537cc71a7bfb6061607263a1eecc81a
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-12-17 22:09:57 +0530

    cors added

--- index.js
+++ index.js
@@ -14,6 +14,15 @@ const router = jsonServer.router(jsonData);
 
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+}
+
+server.use(cors(corsOptions));
+
 server.use(router);
 
 server.listen(port, () => {
