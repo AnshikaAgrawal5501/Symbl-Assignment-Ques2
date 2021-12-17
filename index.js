f177f0b575da63638de5d76f00ed0355d364b6b4
AnshikaAgrawal5501 <anshikaagrawal5501@gmail.com>
2021-12-17 20:55:45 +0530

    items sorted as per id

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const jsonData = require('./db.json');
+
+const data = jsonData.data.map((item) => {
+    return item;
+}).sort((a, b) => {
+    return a.id - b.id;
+});
+
+jsonData.data = data;
+
+const router = jsonServer.router(jsonData);
 
 const port = process.env.PORT || 3000;
 
 server.use(router);
-server.listen(port);
+
+server.listen(port, () => {
+    console.log(`App running at http://localhost:${port}/data`);
+});
